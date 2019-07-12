7c174f25559982e6ae4c7c59ccfd8e941b2019f7
Jennarong <jennarong.s@scale360solutions.com>
2019-07-12 17:03:06 +0700

    update fonts

--- src/constants/fonts.js
+++ src/constants/fonts.js
@@ -4,4 +4,15 @@ export default {
     OpenSansCondensedLightItalic: 'OpenSansCondensed-LightItalic',
     QuarkBold: 'Quark-Bold',
     QuarkLight: 'Quark-Light',
+    KanitBlack: 'Kanit-Black',
+    KanitBold: 'Kanit-Bold',
+    KanitExtraBold: 'Kanit-ExtraBold',
+    KanitExtraLight: 'Kanit-ExtraLight',
+    KanitLight: 'Kanit-Light',
+    KanitMedium: 'Kanit-Medium',
+    KanitRegular: 'Kanit-Regular',
+    KanitRegularItalic: 'Kanit-RegularItalic',
+    KanitSemiBold: 'Kanit-SemiBold',
+    KanitThin: 'Kanit-Thin',
+    
 }
